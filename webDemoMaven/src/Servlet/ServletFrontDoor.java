fe7e47f7fbf858f21484044edf98fad9ee4ba3bb
JudeHuang2004 <JudeHuang2004@gmail.com>
2017-07-10 11:18:57 -0500

    Add files via upload

--- webDemoMaven/src/Servlet/ServletFrontDoor.java
+++ webDemoMaven/src/Servlet/ServletFrontDoor.java
@@ -1,1 +1,36 @@
-ettst
+package Servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class ServletFrontDoor extends HttpServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1366135174023953879L;
+
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// ---- forward to private page
+		ServletConfig config = getServletConfig();
+		ServletContext context = config.getServletContext();
+
+		String requestURI = request.getRequestURI();
+		if (requestURI.contains("fd/public")) {
+			RequestDispatcher rd = context.getRequestDispatcher("/private");
+			rd.forward(request, response);
+		} else {
+			RequestDispatcher rd = context.getRequestDispatcher("/error");
+			rd.forward(request, response);
+		}
+	}
+}
